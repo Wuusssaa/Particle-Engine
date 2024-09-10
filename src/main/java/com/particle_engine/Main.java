8f167e7b4651bf54e14b4db25849f877a1693d82
wuusssaa <vanessahaynesusa@gmail.com>
2024-09-10 16:47:42 -0500

    first commit for particle engine

---
+++ src/main/java/com/particle_engine/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Coder: Vanessa Haynes
+ * Project: Project 1 - Particle Engine
+ * Date: Sep. 2024
+ * Description: Particle engine with basic physics and some interactivity
+ */
+
+package com.particle_engine;
+import processing.core.*;
+
+public class Main extends PApplet {
+
+//sets up processing and prints the hello world message to the console
+    public static void main(String[] args) {
+        PApplet.main("com.particle_engine.Main");
+    }
+
+    public void settings() {
+        size (700,500);
+    }
+}
